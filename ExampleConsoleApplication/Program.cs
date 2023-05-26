1ee1d82c8a8aca09fc04d9e2dbaf1056b42de4ce
Md. Sayeed Rahman <mdsayeedrahman1999@gmail.com>
2023-05-27 03:01:05 +0600

    added console application as a very basic example project

---
+++ ExampleConsoleApplication/Program.cs
@@ -0,0 +1,28 @@
+﻿using Sayeed.NETCore.DataProtection;
+
+// declaring array of keywords that should be hidden from entity.
+string[] keywords = new string[] { "phone", "email" };
+
+// initializing an instance of helper class with customly defined keywords.
+var helper = new Helper(keywords);
+
+// creating mock entity to hide properties from.
+var entity = new MockClass
+{
+    Name = "mock class",
+    Phone = "880000000000",
+};
+
+// hiding sensative properties from original object.
+helper.HideSensativeProperties(entity);
+
+Console.WriteLine($"Name = {entity.Name}");
+Console.WriteLine($"Phone = {entity.Phone}");
+Console.WriteLine("Press any key to exit.");
+Console.ReadKey();
+
+class MockClass
+{
+    public string Name { get; set; }
+    public string Phone { get; set; }
+}
